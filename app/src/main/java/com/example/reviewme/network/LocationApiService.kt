d1c5a258673a078279628471e538d5b09b94d5b6
Mihai Predoiu <predoiumihai@gmail.com>
2022-01-18 01:45:50 +0200

    Call Google Maps API on search

---
+++ app/src/main/java/com/example/reviewme/network/LocationApiService.kt
@@ -0,0 +1,28 @@
+package com.example.reviewme.network
+
+
+import retrofit2.Retrofit
+import retrofit2.Call
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://maps.googleapis.com"
+
+private val API_KEY =  System.getenv("REVIEW_ME_API_KEY")
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface LocationApiService {
+    @GET("maps/api/place/textsearch/json")
+    fun getLocations(@Query("query") query: String?, @Query("key") key: String = API_KEY): Call<String>
+}
+
+object LocationApi {
+    val retrofitService : LocationApiService by lazy {
+        retrofit.create(LocationApiService::class.java)
+    }
+}
--- app/src/main/java/com/example/reviewme/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/reviewme/ui/home/HomeViewModel.kt
@@ -3,6 +3,11 @@ package com.example.reviewme.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.reviewme.network.LocationApi
+import retrofit2.Call
+import retrofit2.Response
+import java.net.URLEncoder
+import javax.security.auth.callback.Callback
 
 class HomeViewModel : ViewModel() {
 
@@ -18,7 +23,21 @@ class HomeViewModel : ViewModel() {
     fun getResults(query: String?) {
         if (query !== null) {
             System.out.println("I want to get results for $query")
+            getHomeLocations(query)
         }
+    }
+
+    private fun getHomeLocations(query: String?) {
+        LocationApi.retrofitService.getLocations(URLEncoder.encode(query, "utf-8")).enqueue(
+            object: retrofit2.Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    locations.value = listOf(response.body().toString().take(50), "C", "D", "S")
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    locations.value = listOf("E", "R", "R", "O", "R")
+                }
+            })
     }
 
 }
