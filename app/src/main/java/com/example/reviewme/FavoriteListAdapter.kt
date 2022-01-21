43312700578257a76928b4b37b01d0eac5e0c2dc
Mihai Predoiu <predoiumihai@gmail.com>
2022-01-21 17:04:02 +0200

    Update the general flow

---
+++ app/src/main/java/com/example/reviewme/FavoriteListAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.reviewme
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.reviewme.classes.Location
+
+class LocationsSearchAdapter(
+    private val onItemClicked: (Location) -> Unit
+): RecyclerView.Adapter<LocationItemViewHolder>() {
+
+    var data: List<Location> = listOf()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+
+        return LocationItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LocationItemViewHolder, position: Int) {
+        holder.itemTitle.text = data[position].name
+        holder.itemDescription.text = data[position].formatted_address
+        holder.itemRating.text = data[position].rating.toString()
+        holder.itemStatus.text = data[position].status
+
+        if (data[position].status == "Closed") {
+            holder.itemStatus.setTextColor(Color.rgb(222, 1, 23))
+        }
+
+        val item = data[position]
+        holder.locationView.setOnClickListener{
+            onItemClicked(item)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+}
+
+class LocationItemViewHolder(val locationView: View): RecyclerView.ViewHolder(locationView) {
+    var itemTitle : TextView = locationView.findViewById(R.id.item_title)
+    var itemDescription: TextView = locationView.findViewById(R.id.item_description)
+    var itemRating: TextView = locationView.findViewById(R.id.item_rating)
+    var itemStatus: TextView = locationView.findViewById(R.id.item_status)
+
+}
