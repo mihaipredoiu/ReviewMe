96492cb9f6b9008762c10cf8a74295b0baa376c9
Mihai Predoiu <predoiumihai@gmail.com>
2022-01-21 18:19:37 +0200

    Improve coding-style

--- app/src/main/java/com/example/reviewme/network/LocationApiService.kt
+++ app/src/main/java/com/example/reviewme/network/LocationApiService.kt
@@ -1,15 +1,15 @@
 package com.example.reviewme.network
 
-
-import retrofit2.Retrofit
 import retrofit2.Call
+import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 private const val BASE_URL = "https://maps.googleapis.com"
 
-private val API_KEY =  System.getenv("REVIEW_ME_API_KEY") ?: "AIzaSyAZuwaDgYQkLe-uOBtJEbtMS_n3_Fd6SiM"
+private val API_KEY =
+    System.getenv("REVIEW_ME_API_KEY") ?: "AIzaSyAZuwaDgYQkLe-uOBtJEbtMS_n3_Fd6SiM"
 
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
@@ -18,14 +18,20 @@ private val retrofit = Retrofit.Builder()
 
 interface LocationApiService {
     @GET("maps/api/place/textsearch/json")
-    fun getLocations(@Query("query") query: String?, @Query("key") key: String = API_KEY): Call<String>
+    fun getLocations(
+        @Query("query") query: String?,
+        @Query("key") key: String = API_KEY
+    ): Call<String>
 
     @GET("maps/api/place/details/json")
-    fun getLocationById(@Query("place_id") id: String, @Query("key") key: String = API_KEY): Call<String>
+    fun getLocationById(
+        @Query("place_id") id: String,
+        @Query("key") key: String = API_KEY
+    ): Call<String>
 }
 
 object LocationApi {
-    val retrofitService : LocationApiService by lazy {
+    val retrofitService: LocationApiService by lazy {
         retrofit.create(LocationApiService::class.java)
     }
 }
